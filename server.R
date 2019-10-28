5d8c2920e9d23179b6ae5242fd9b8d0f3ca9ad60
claesbackman <claes.backman@gmail.com>
2019-10-28 20:28:04 +0100

    Fixade hyreskostnad per måand

--- server.R
+++ server.R
@@ -1,24 +1,30 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
-
-# definera saker 
+library(scales)
 
 
 shinyServer(function(input, output) {
         
         ################################ 
-        # Random 
+        # Att göra 
         ################################ 
   
-    # Att göra 
-        # uppdatera procent i amorteringar enligt kontantinsats och amorteringskravet
-        
+        # uppdatera procent i amorteringar enligt andra amorteringskravet
+        # Skatt på investeringar
+        # Topplån
+        # Ändra så att amorteringar/ränta är noll om skulden är noll
+  
+        # Just nu: renoveringar uppdateras inte när priset går upp. Så du renoverar en procent av det ursprungliga beloppet
+          # Annars så ökar renoveringarna när huspriserna ökar, vilket känns konstigt 
+  
         ################################
         # initiala kostnader köpa 
+        ################################
+  
         formel_intiala_köpa <-  reactive({
                 pris <- input$boxPris
-                ki <- input$boxKI        
+                ki <- input$boxKI / 100    
                 round(pris*ki)
                 })
         
@@ -26,15 +32,15 @@ shinyServer(function(input, output) {
         # Återkommande kostnader
         ################################
         
-        # Räntekostnader 
+        # Räntekostnader
         räntaTable <- reactive({
           tid <- input$boxTid
           tid2 <- tid-1
           pris <- input$boxPris
-          ki <- input$boxKI
-          amortering <- input$BoxAmort
-          ränta <- input$boxR*0.7 # ränte gånger 1-skatteavdrag 
+          ki <- input$boxKI / 100 
+          amortering <- input$BoxAmort / 100 
           skuld <- pris*(1-ki)
+          ränta <- input$boxR*0.7  / 100 # ränte gånger 1-skatteavdrag 
           df  <- skuld*ränta
           for (i in 1:tid2) {
             bal <- skuld*(1-amortering*i)*ränta
@@ -47,22 +53,27 @@ shinyServer(function(input, output) {
         räntaSum <- reactive({sum(räntaTable())
         })
         
-        # renoveringar - räknas med en geometrisk formel. Procent av tidigare års bostadspris
+        # Renoveringar - procent av tidigare års bostadspris
+          # renoveringar <- reactive({
+          #   renovering <-  input$boxReno * input$boxPris / 100 
+          #   tid <- input$boxTid
+          #   ökning <- 1+input$boxDeltaP / 100 
+          #   round(renovering * (1-ökning^tid)/(1-ökning))
+          # })
+          # 
+        
+        # Renoveringar - procent av ursprunliga priset
         renoveringar <- reactive({
-          renovering <-  input$boxReno * input$boxPris
-          tid <- input$boxTid
-          ökning <- 1+input$boxDeltaP
-          renovering * (1-ökning^tid)/(1-ökning)
+          input$boxReno * input$boxPris / 100 
         })
         
         # totala återkommande kostnader 
         formel_totala_återkommande <- reactive({
-          avgifter <- input$boxAvgift
-          försäkring <- input$boxFörsäkring
+          avgifter <- input$boxAvgift * 12
+          försäkring <- input$boxFörsäkring * 12
           tid <- input$boxTid
-          andra <- input$boxAndraKöpa
-          # skatter 
-          round(renoveringar() + räntaSum() + (försäkring+andra)*tid,digit=0)
+          andra <- input$boxAndraKöpa * 12
+          round(renoveringar() + räntaSum() + (försäkring+andra+avgifter)*tid,digit=0)
         })
         
         ################################
@@ -71,13 +82,14 @@ shinyServer(function(input, output) {
         
         # kontantinsats + amorteringar investerat istället
           # Nu räknas det som att betalningar sparas i slutet av året.
+        
         formel_alternativkostnad <- reactive({
-          ki <- input$boxKI *input$boxPris
-          skuld <- (1-input$boxKI) *input$boxPris
-          r_stocks <- input$bpxDeltaSM
+          kontantinsats_kr <- input$boxKI / 100  *input$boxPris
+          skuld <- input$boxPris - kontantinsats_kr
+          r_stocks <- input$boxDeltaSM / 100 
           tid <- input$boxTid
-          betalningar <- input$BoxAmort * skuld #+ input$boxReno + input$boxAndraKöpa + input$boxFörsäkring + input$boxAvgift
-          round(ki*(1+r_stocks)^tid-ki+(betalningar*(((1 + r_stocks)^(tid) - 1) / (r_stocks)))-betalningar*tid, digit=0)
+          betalningar <- input$BoxAmort * skuld / 100  #+ input$boxReno + input$boxAndraKöpa + input$boxFörsäkring + input$boxAvgift
+          round(kontantinsats_kr*(1+r_stocks)^tid-kontantinsats_kr+(betalningar*(((1 + r_stocks)^(tid) - 1) / (r_stocks)))-betalningar*tid, digit=0)
           })
         
         ################################
@@ -86,15 +98,16 @@ shinyServer(function(input, output) {
         
         formel_husprisvinst <-  reactive({
                 pris <- input$boxPris
-                husprisökning <- input$boxDeltaP     
-                round(pris*(1+husprisökning)^input$boxTid-pris, digits=0)
+                husprisökning <- input$boxDeltaP   / 100 
+                prisökning <- pris*(1+husprisökning)^input$boxTid-pris 
+                round(prisökning - (prisökning - renoveringar())*22/30*0.3, digits=0)
         })
     
         # totala amorteringar - amorteringsprocent gånger antal år 
         totamortering <- reactive({
                 pris <- input$boxPris
-                amortering <- input$BoxAmort
-                ki <- input$boxKI
+                amortering <- input$BoxAmort / 100 
+                ki <- input$boxKI / 100 
                 år <- input$boxTid
                 round(pris*amortering*år, digits=2)
                 
@@ -104,30 +117,70 @@ shinyServer(function(input, output) {
         # Sammanställning kostnader för att köpa
         ################################
         
-        formel_totala_kostnader <- reactive ({
+        formel_totala_kostnader <- reactive({
           formel_intiala_köpa() + formel_totala_återkommande() + formel_alternativkostnad() - formel_husprisvinst()
         })
+        
+        
+        ################################
+        # Hyra - 
+        ################################
+        
+        # Hyreskostnad per månad 
+        minHyra <- reactive({
+          hyresökning <- (1+input$boxDeltaRent/100)
+          tid <- input$boxTid
+          AndraHyra <- input$boxAndraHyra
+          hyra <- round(formel_totala_kostnader() / ((1-hyresökning^tid)/(1-hyresökning))/12, digit=3)
+        })
+
+  
+        # # Totala hyreskostader
+        sumHyra <- reactive({
+          hyresökning <- (1+input$boxDeltaRent/100)
+          tid <- input$boxTid
+          AndraHyra <- input$boxAndraHyra
+          # Formel 
+          round(minHyra()*12 * ((1-hyresökning^tid)/(1-hyresökning)), digit=0)
+        })
+
+        ################################ 
+        # Förmögenhet efter x år
+        ################################
+        
+        förmögenhet_köpa <- reactive({
+          formel_husprisvinst() + totamortering()
+        })
+        
+        förmögenhet_hyra <- reactive({
+            pris <- input$boxPris
+            ki <- input$boxKI  / 100 
+            avkastning <- input$boxDeltaSM / 100 
+            tid <- input$boxtid
+            round(pris*(1-ki)*(1+avkastning)^tid)
+              })
         
         ################################ 
         # Grafer
         ################################
         
-        # Räntekostnader 
+        # Totala kostnader 
         kostnadTable <- reactive({
           tid <- input$boxTid
           tid2 <- tid-1
           pris <- input$boxPris
-          deltaP <- (1+input$boxDeltaP)
-          ki <- input$boxKI
-          amortering <- input$BoxAmort
-          ränta <- input$boxR*0.7 # ränte gånger 1-skatteavdrag 
+          deltaP <- (1+input$boxDeltaP / 100 )
+          ki <- input$boxKI / 100 
+          amortering <- input$BoxAmort / 100 
+          ränta <- input$boxR*0.7 / 100  # ränte gånger 1-skatteavdrag 
           skuld <- pris*(1-ki)
-          reno <- input$boxReno
-          andrasammanlagda <- input$boxAvgift +input$boxFörsäkring + input$boxAndraKöpa 
+          reno <- input$boxReno / 100 
+          andrasammanlagda <- input$boxAvgift * 12 +input$boxFörsäkring * 12 + input$boxAndraKöpa  * 12
           df2  <- (skuld*ränta + reno*pris + andrasammanlagda)/12
-          # Räntekostnader + renoveringar + sammanställda * inflationstakt
+          
+          # Räntekostnader + renoveringar + sammanställda 
             for (i in 1:tid2) {
-              bal2 <- (skuld*(1-amortering*i)*ränta + reno*pris*deltaP^i + andrasammanlagda)/12
+              bal2 <- (skuld*(1-amortering*i)*ränta + reno*pris + andrasammanlagda)/12
               df2 <- rbind(df2,bal2)
             }
           print(df2[,1])
@@ -138,31 +191,105 @@ shinyServer(function(input, output) {
           tid <- input$boxTid
           tid2 <- tid-1
           pris <- input$boxPris
-          deltaP <- (1+input$boxDeltaP)
-          ki <- input$boxKI
-          amortering <- input$BoxAmort
-          ränta <- input$boxR*0.7 # ränte gånger 1-skatteavdrag 
+          deltaP <- (1+input$boxDeltaP / 100 )
+          ki <- input$boxKI / 100 
+          amortering <- input$BoxAmort / 100 
+          ränta <- input$boxR*0.7 / 100  # ränte gånger 1-skatteavdrag 
           skuld <- pris*(1-ki)
-          reno <- input$boxReno
-          andrasammanlagda <- input$boxAvgift +input$boxFörsäkring + input$boxAndraKöpa 
+          reno <- input$boxReno / 100 
+          andrasammanlagda <- input$boxAvgift * 12+input$boxFörsäkring * 12+ input$boxAndraKöpa * 12
           df3  <- (skuld*ränta + reno*pris + andrasammanlagda + amortering*pris*(1-ki))/12
             for (i in 1:tid2) {
-              bal3 <- (skuld*(1-amortering*i)*ränta + reno*pris*deltaP^i + andrasammanlagda + amortering*pris*(1-ki))/12
+              bal3 <- (skuld*(1-amortering*i)*ränta + reno*pris + andrasammanlagda + amortering*pris*(1-ki))/12
               df3 <- rbind(df3,bal3)
             }
           print(df3[,1])
         })
-
         
-          # Graf med kostnader per månad och utgifter per månad
-           output$plot1 <- renderPlot({
-                   tid <- input$boxTid -1
-                plot(0:tid, utgiftTable(), xlab="År",ylab="Kostnader",col="red")
-                  lines(0:tid, utgiftTable(),col="green")
-            })
+          #Graf med kostnader per månad och utgifter per månad
+          output$plot1 <- renderPlot({
+                  # X axis values
+                  tid <- input$boxTid
+                  if(tid<10) {
+                    tid_x <- tid
+                  }
+                  if(tid>=10){
+                    tid_x <- tid / 2
+                  }
+                  if(tid>=25){
+                    tid_x <- tid / 3
+                  }
+                  # Values for graph
+                  xValue <- 1:tid 
+                  yValue <- utgiftTable()
+                  data <- data.frame(xValue,yValue)
 
-        ################################ 
-        # Uppdelade kostnader 
+                # Define the plot 
+                p <-   ggplot(data, aes(x=xValue, y = yValue))+ 
+                              geom_point( color="#69b3a2", size=4, alpha=0.9) +
+                  geom_line() + 
+                  labs(y=expression(atop("Utgifter", paste("per månad" )))) + 
+                  theme_bw(base_size = 12) + 
+                  theme(
+                        panel.grid.major = element_blank(), 
+                        panel.border = element_blank(),
+                        axis.ticks = element_blank(),
+                        axis.title.y.right = element_text(angle = 0, vjust=1, face = "bold"),
+                        axis.title.x = element_text(face = "bold")
+                        ) +
+                  scale_y_continuous(position = "right") + 
+                  scale_x_continuous("År",breaks= pretty_breaks(n = tid_x)) 
+                
+                # Print the plot 
+                print(p)
+           })
+
+          output$plotPris <- renderPlot({
+            
+            # definera boxarna 
+            tid <- input$boxTid
+            pris <- input$boxPris
+            deltaP <- (1+input$boxDeltaP / 100 )
+            ki <- input$boxKI / 100
+            amortering <- input$BoxAmort / 100
+            ränta <- input$boxR*0.7 / 100  # ränte gånger 1-skatteavdrag 
+            skuld <- pris*(1-ki)
+            reno <- input$boxReno / 100 
+            andrasammanlagda <- input$boxAvgift * 12+input$boxFörsäkring * 12+ input$boxAndraKöpa * 12
+            
+            # X axis values
+            pris <- input$boxPris
+          
+            # Values for graph
+            xValue <- 1000000
+            yValue <- (skuld*ränta + reno*pris + andrasammanlagda + amortering*pris*(1-ki))/12
+
+            data <- data.frame(xValue,yValue)
+            
+            # Define the plot 
+            p <-   ggplot(data, aes(x=xValue, y = yValue))+ 
+              geom_point(color="#69b3a2", size=4, alpha=0.9) +
+              geom_line() + 
+              labs(y=expression(atop("Utgifter", paste("per månad" )))) + 
+              theme_bw(base_size = 12) + 
+              theme(
+                panel.grid.major = element_blank(), 
+                panel.border = element_blank(),
+                axis.ticks = element_blank(),
+                axis.title.y.right = element_text(angle = 0, vjust=1, face = "bold"),
+                axis.title.x = element_text(face = "bold")
+              ) +
+              scale_y_continuous(position = "right") + 
+              scale_x_continuous("År",breaks= pretty_breaks()) 
+            
+            # Print the plot 
+            print(p)
+          })
+          
+          
+    
+        ################################
+        # Uppdelade kostnader
         ################################ 
 
         # Titeltext
@@ -198,10 +325,22 @@ shinyServer(function(input, output) {
           paste("Totala kostnader:", formel_totala_kostnader(), "kr")
         })
         
+        output$totala_kostnader_hyra <- renderText({
+          paste("Kostnad for att hyra:", sumHyra(), "kr")
+        })
+        
+     
+        
+        
+        
+        
+        
         
         ################################ 
         # texter 
         ################################ 
+        
+        # header
         
         # Räntekostnader
         output$räntekostnader <- renderText({
@@ -220,22 +359,52 @@ shinyServer(function(input, output) {
         
         # Bostadspris
         output$Text_pris <- renderText({
-                "<b> Bostadspris. </b> En väldigt viktig del är hur din nya bostad kostar. 
-                Det är inte den enda delen, utan vi behöver titta på fler saker innan vi kan ta ett beslut."
-        })
+                "<h4> Hur mycket kostar bostaden? </h4> En väldigt viktig del är hur din nya bostad kostar. 
+                Här räknar vi med både priset för att köpa och eventuella avgifter till en bostadsrättsförening. "        })
+      
         
         # Tid i bostaden 
         output$text_tid <- renderText({
-                "<b> Tid i bostaden </b>. Ju längre du planerar att stanna, desto bättre är det att köpa. 
+                "<h4> Hur länge planerar du att bo kvar? </h4> Ju längre du planerar att stanna, desto bättre är det att köpa. 
                               Det är för att fasta kostnader sprids över fler år. "
+        })
+        
+        # Bolån
+        output$text_bolån <- renderText({
+          "<h4> Hur ser ditt bolån ut?</h4> Vad din ränta och kontantinsats är har stor påverkan på din månadskostnad."
+        })
+        
+        belåningsgrad <- reactive({
+          100-input$boxKI
+                  })
+        
+        output$text_belåningsgrad <- renderText({
+          paste("<b> Med en kontantinsats på", input$boxKI," blir din belåningsgrad", belåningsgrad(), "procent. </b>")
         })
       
         #inkomst
-        output$text_inkomst <- renderText({
-                "<b> Inkomst </b>. Om ditt bolån är större än 4.5 gånger din bruttoinkomst
-                ska du amortera en procent mer per år. 
-                Din bruttoinkomst beräknas genom att XXX..."
+        output$text_amortering <- renderText({
+                "<h4> Hur mycket ska du amortera? </h4> Dina amorteringsbetalningar beror på din belåningsgrad och hur stor 
+          din inkomst är relativt ditt lån. Du kan också välja att amortera mer, då minskar dina lånekostnad snabbare över tid."
         })
+        
+        output$text_framtiden <- renderText({
+          "<h4> Vad händer i framtiden? </h4> Vad som händer i framtiden har stor påverkan, men det är tyvärr
+          väldigt svårt att veta vad som kommer hända i framtiden. 
+          Vi har fyllt i värden för husprisökningar, hyresökningar, avkastningar på investeringar, och inflation.
+          Om du själv vill fylla i värden för framtida huspriser, hyresökningar, avkastning på investeringar 
+          och inflation så klicka i boxen nedan. " 
+        })
+        output$text_renoveringar <- renderText({
+          "<h4> Avgifter, renoveringar, försäkringar och andra kostnader.</h4> Här kan du fylla i andra kostnader
+            som kommer när du köper. " 
+        })
+        
+        # output$text_hyra <- renderText({
+        #   "<b> Kostnader för att hyra.</b> " 
+        # })
+        
+        
         
 })
 
--- ui.R
+++ ui.R
@@ -1,50 +1,119 @@
 library(shiny)
 library(shinythemes)
+library(markdown)
 
 shinyUI(
-    fluidPage(theme=shinytheme("united"),
+    fluidPage(
+      tags$head(
+        tags$style(HTML("
+
+     .affix {
+        position:fixed;
+      }
+
+    "))
+      ),
+      theme=shinytheme("sandstone"),
+             
+      fluidRow(
+        column(8,
+                 p(img(src="LU_Ekonomihögskolan_RGB_SV.png",height="30%",width="30%"),
+                    HTML("<hr color='blue' >") 
+                  )
+        ),
+        column(3, p("Klicka här för att komma till kursen och läsa mer. "))
+      ),
       titlePanel("Ska du köpa eller hyra?"),
-  
+#       headerPanel("Ska du köpa eller hyra?"),
+      
+
     # huvudpanel
-    fluidRow(column(4,
+    fixedRow(column(8, 
+                    p("Det är mycket att tänka på när du ska köpa en bostad, 
+                      och det kan vara svårt att räkna ut om det är bättre att hyra eller att köpa. 
+                      För att hjälpa dig med det beslutet så tar verktyget nedan in de viktigaste kostnaderna 
+                      för att köpa och beräknar en motsvarande hyra. 
+                      Om du kan hitta en bostad för en lägre hyra än det så är det bättre att hyra." 
+                    , HTML("<hr color='blue' >")),
                     htmlOutput("Text_pris"),
-                    sliderInput("boxPris", ""
-                                , value = 1000000, min=0, max=10000000, step=10000, ticks=FALSE, width="95%"),  
+                    br(),            
+                    
+                    fluidRow(column(2,
+                                    sliderInput("boxPris", "Bostadspris"
+                                                , value = 1000000, min=0, max=10000000, step=10000, ticks=FALSE, width="95%", post  = " kr")
+                                    ),
+                             column(10,
+                                    plotOutput("plotPris", height="300px", width= "100%")
+                             )
+                    ),
+                    
+                    br(),            
+                    HTML("<hr color='blue' >"),
                     htmlOutput("text_tid"),
-                    sliderInput("boxTid", ""
-                                , value = 10, min=1, max=50, step=1, ticks=FALSE, width="95%"),
-                    htmlOutput("text_inkomst"),
-                    sliderInput("boxInc", "", value = 200000, min=0, max=2000000, step=1000, ticks=FALSE, width="95%"),
-                    sliderInput("boxR", "Ränta.", value = .03, min=.00, max=0.15, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxRtopp", "Ränta på eventuellt topplån.", value = .05, min=.01, max=0.15, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxAvgift", "Avgift till förening", value = 2000, min=0, max=15000, step=100, ticks=FALSE, width="95%"),
-                    sliderInput("boxKI", "Kontantinsats", value = .15, min=.15, max=1, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("BoxAmort", "Amortering", value = 0.02, min=0.02, max=1, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxDeltaP", "Uppskattad husprisökning", value = 0.04, min=0, max=1, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxDeltaRent", "Uppskattad hyresökning", value = 0.02, min=0, max=1, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("bpxDeltaSM", "Uppskattad avkastning på investeringar", value = 0.07, min=0, max=1, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxInflation", "Uppskattad inflation", value = 0.02, min=0, max=1, step=.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxReno", "Renoveringar", value = 0.01, min=0, max= 0.15, step=0.01, ticks=FALSE, width="95%"),
-                    sliderInput("boxFörsäkring", "Försäkring", value = 1000, min=0,  max= 100000,step=100, ticks=FALSE, width="95%"),
-                    sliderInput("boxAndraKöpa", "Andra kostnader", value = 1000, min=0,  max= 100000, step=100, ticks=FALSE, width="95%"),
-                    sliderInput("box13", "Andra kostnader för att hyra", value = 1000, min=0,  max= 100000, step=100, ticks=FALSE, width="95%"),
-                    sliderInput("box14", "Försäkring", value = 1000, min=0,  max= 100000, step=100, ticks=FALSE, width="95%")
+                    br(),            
+                    
+                    sliderInput("boxTid", "Tid i bostaden"
+                                , value = 10, min=1, max=50, step=1, ticks=FALSE, width="95%", post  = " år"),
+                    htmlOutput("text_bolån"),
+                    
+                    sliderInput("boxR", "Bolåneränta.", value = 3, min=0, max=15, step=0.1, ticks=FALSE, width="95%", post  = " %"),
+                    # sliderInput("boxRtopp", "Ränta på eventuellt topplån.", value = 5, min=0, max=15, step=0.1, ticks=FALSE, width="95%", post  = " %"),
+                    sliderInput("boxKI", "Kontantinsats. ", value = 15, min=15, max=100, step=1, ticks=FALSE, width="95%", post  = " %"),
+                    htmlOutput("text_belåningsgrad"),
+                    htmlOutput("text_amortering"),
+                    sliderInput("boxInc", "Bruttoinkomst", value = 200000, min=0, max=2000000, step=1000, ticks=FALSE, width="95%", post  = " kr"),
+                      conditionalPanel(
+                      condition = "input.boxKI <30",
+                      sliderInput("BoxAmort", "Amortering", value = 2, min=2, max=100, step=1, ticks=FALSE, width="95%", post  = " %")
+                    ),
+                    conditionalPanel(
+                      condition = "input.boxKI <50 & input.boxKI>=30 ",
+                      sliderInput("BoxAmort", "Amortering", value = 1, min=1, max=100, step=1, ticks=FALSE, width="95%", post  = " %")
+                    ),
+                    conditionalPanel(
+                      condition = "input.boxKI >=50",
+                      sliderInput("BoxAmort", "Amortering", value = 0, min=0, max=100, step=1, ticks=FALSE, width="95%", post  = " %")
+                    ),
+                    # sliderInput("BoxAmort", "Amortering", value = 2, min=0, max=100, step=1, ticks=FALSE, width="95%", post  = " %"),
+                    htmlOutput("text_framtiden"),
+                    checkboxInput("framtiden", "Välj värden själv"),
+                    conditionalPanel(
+                      condition = "input.framtiden == true",
+                        sliderInput("boxDeltaP", "Husprisökning", value = 4, min=0, max=100, step=1, ticks=FALSE, width="95%", post  = " %"),
+                        sliderInput("boxDeltaRent", "Hyresökning", value = 2, min=0, max=100, step=1, ticks=FALSE, width="95%", post  = " %"),
+                        sliderInput("boxDeltaSM", "Avkastning på investeringar", value = 7, min=0, max=100, step=1, ticks=FALSE, width="95%", post  = " %"),
+                        sliderInput("boxInflation", "Inflation", value = 2, min=0, max=100, step=1, ticks=FALSE, width="95%", post  = " %")
+                    ),
+                    htmlOutput("text_renoveringar"),
+                    sliderInput("boxAvgift", "Avgift till förening", value = 2000, min=0, max=15000, step=100, ticks=FALSE, width="95%", post  = " kr"),
+                    sliderInput("boxReno", "Renoveringar", value = 1, min=0, max= 15, step=0.1, ticks=FALSE, width="95%", post  = " %"),
+                    sliderInput("boxFörsäkring", "Försäkring", value = 1000, min=0,  max= 100000,step=100, ticks=FALSE, width="95%", post  = " kr"),
+                    sliderInput("boxAndraKöpa", "Andra kostnader", value = 1000, min=0,  max= 100000, step=100, ticks=FALSE, width="95%", post  = " kr"),
+                    sliderInput("boxAndraHyra", "Andra kostnader för att hyra", value = 10000, min=0,  max= 100000, step=100, ticks=FALSE, width="95%", post  = " kr")
+                    
+                    # 
+                    # checkboxInput("smooth", "Smooth"),
+                    # conditionalPanel(
+                    #   condition = "input.smooth == true",
+                    #   selectInput("smoothMethod", "Method",
+                    #               list("lm", "glm", "gam", "loess", "rlm"))
+                    # )
                     #sliderInput("boxHyra", "Hyra", value = 1000, min=0,  max= 100000, step=100, ticks=FALSE, width="95%")
                     ), #  column
              
-             h1("Här summerar vi alla kostnader"),
-             column(4, 
+             # column(5,
+             #        plotOutput("plot1", width= "75%")
+             #        ),
+             column(1,  fixedPanel(top = "100", width = "45%",
                     htmlOutput("sumkostnader"),
                     textOutput("intiala_köpa"),
                     textOutput("återkommande"),
                     textOutput("alternativkostnader"),
                     textOutput("vinster"),
-                    textOutput("totala_kostnader_köpa"),
-                    # textOutput("amorteringar"),
-                    # textOutput("fasta"),
-                    # textOutput("räntekostnader"),
-                   plotOutput("plot1",width= "100%")
-             ) # Column 
+                    textOutput("totala_kostnader_köpa"), 
+                    br(),
+                    textOutput("totala_kostnader_hyra")
+                   ))
         ) # fluidrow 
   ) # fluidpage 
     
